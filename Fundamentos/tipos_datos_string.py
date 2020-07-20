64bf9d6838a46f4028c6c6af59ce3543ecc2c08d
algonca00 <algonca00@gmail.com>
2020-07-20 15:21:45 -0400

    02. Variables en Python - 5. Tipos String(Cadena) en Python

---
+++ Fundamentos/tipos_datos_string.py
@@ -0,0 +1,13 @@
+cadena = "aerosmith"
+print(cadena)
+print("Mi grupo favorito es: " + cadena)
+
+numero1 = "1"
+numero2 = "2"
+print("Concatenacion: " + numero1 + numero2)
+#Los string se pueden concatenar con el signo +
+
+num1 = 1
+num2 = 2
+print("Operacion suma: ", num1 + num2)
+#No se pueden concatenar string con variables numericas pero si se pueden unir por medio de la ,  

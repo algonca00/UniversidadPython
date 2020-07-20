7b50973a87b238e6f0293fdc840d5b5095385d74
algonca00 <algonca00@gmail.com>
2020-07-20 18:42:08 -0400

    02. Variables en Python - 8. Entrada de Datos en Visual Studio Code

--- Fundamentos/entrada_datos.py
+++ Fundamentos/entrada_datos.py
@@ -1,10 +1,10 @@
 #Usamos la funcion input() para la entrada de datos y le asignamos la entrada a una variable
-resultado = input()
+resultado = input("Ingresa una palabra por favor!!!:  ")
 print(resultado)
 
 # La funcion input() nos regresa un string, osea que si queremos realizar operaciones, cambiamos el tipo con la funcion int
-num1 = int(input())
+num1 = int(input("Ingresa un valor numerico: "))
 print("num1 es igual a:",num1)
-num2 = int(input())
+num2 = int(input("Ingresa otro valor numerico: "))
 print("num2 es igual a:",num2)
 print("Y el resultado de num1 + num2 es:",num1+num2)

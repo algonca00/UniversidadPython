848e6d5f7b6128d8d5a291038989ff46515b4b6d
algonca00 <algonca00@gmail.com>
2020-07-20 18:23:26 -0400

    02. Variables en Python - 7. Entrada de Datos en Python

---
+++ Fundamentos/entrada_datos.py
@@ -0,0 +1,10 @@
+#Usamos la funcion input() para la entrada de datos y le asignamos la entrada a una variable
+resultado = input()
+print(resultado)
+
+# La funcion input() nos regresa un string, osea que si queremos realizar operaciones, cambiamos el tipo con la funcion int
+num1 = int(input())
+print("num1 es igual a:",num1)
+num2 = int(input())
+print("num2 es igual a:",num2)
+print("Y el resultado de num1 + num2 es:",num1+num2)

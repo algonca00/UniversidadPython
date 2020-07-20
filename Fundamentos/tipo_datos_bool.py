8f7bd3a684a35b7df490a5e9ee7d496b55c02915
algonca00 <algonca00@gmail.com>
2020-07-20 15:56:05 -0400

    02. Variables en Python - 6. Tipo Bool (Verdadero o Falso) en Python

---
+++ Fundamentos/tipo_datos_bool.py
@@ -0,0 +1,25 @@
+x = True
+print(x)
+
+y = False
+print(y)
+
+num1 = 1
+num2 = 2
+resultado = num1 > num2
+print(resultado)
+resultado = num1 < num2
+print(resultado)
+
+if num1 < num2:
+    print("El valor de num1 es:",num1, "osea SI es menor que num2 que es:",num2)
+else:
+    print("El valor de num1 es:",num1, "osea NO es menor que num2 que es:",num2)
+    
+num1 = 3
+num2 = 2
+
+if num1 < num2:
+    print("El valor de num1 es:",num1, "osea SI es menor que num2 que es:",num2)
+else:
+    print("El valor de num1 es:",num1, "osea NO es menor que num2 que es:",num2)

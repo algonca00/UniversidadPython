b785a21451a66a1eec34713885ce5acde62f83cc
algonca00 <algonca00@gmail.com>
2020-07-19 23:03:40 -0400

    02. Variables en Python - 3. Tipos de datos basicos en Python

---
+++ Fundamentos/tipos_datos.py
@@ -0,0 +1,15 @@
+# Declaramos la variable x como int o entero
+x = 5
+# Declaramos la variable y como float o flotante
+y = 10.1
+# Declaramos la variable z como bool o boleano
+z = False
+# Declaramos la variable a como str o string
+a = "Saludos"
+
+print(x)
+print(y)
+print(z)
+print(a)
+
+# Si queremos saber de que tipo es una variable lo hacemos desde la consola de Python usando la funcion: type(nombre_de_la_variable), en este caso: type(x) type(y) type(z) type(a)

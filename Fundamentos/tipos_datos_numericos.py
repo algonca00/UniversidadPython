f8c5210787ff2d9216b9323f8328c3b9546912df
algonca00 <algonca00@gmail.com>
2020-07-20 14:24:56 -0400

    02. Variables en Python - 4. Tipos Numéricos en Python

---
+++ Fundamentos/tipos_datos_numericos.py
@@ -0,0 +1,10 @@
+x = 10
+print(x)
+# al consultar el tipo de variable con la funcion type(x), obtenemos que es: <class 'int'> osea tipo entero
+y = 15.10
+# al consultar el tipo de variable con la funcion type(y), obtenemos que es: <class 'float'> osea tipo flotante
+z = None
+# al consultar el tipo de variable con la funcion type(z), obtenemos que es: <class 'NoneType'> osea de nimgun tipo, por no haber asignado un valor a esta variable
+print(x)
+print(y)
+print(z)

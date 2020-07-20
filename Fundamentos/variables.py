8843eada9b148016c220847c1eee4d45fef01102
algonca00 <algonca00@gmail.com>
2020-07-19 23:01:20 -0400

    02. Variables en Python - 3. Tipos de datos basicos en Python

--- Fundamentos/variables.py
+++ Fundamentos/variables.py
@@ -1,3 +1,4 @@
+# Declaramos la variable x
 x = 3
 print(x)
 print(x)
@@ -6,10 +7,13 @@ x = 5
 print(x)
 print(x)
 print(x)
+# Declaramos la Variable y
 y = 3
+# Declaramos la variable z asignandole la suma de 2 variables
 z = x + y
 print(x)
 print(y)
 print(z)
+# Declaramos la variable w asignadole como valor otra variable
 w = z
 print(w)
